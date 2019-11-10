a66fba7ba30be3d8715acbb0565631d713b25099
yasuraha <sabu9227@gmail.com>
2019-11-10 14:33:08 +0900

    Improve

--- index.js
+++ index.js
@@ -2,6 +2,9 @@
 const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 
+// APIコールのためのクライアントインスタンスを作成
+const bot = new line.Client(line_config);
+
 // パラメータ設定
 const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
@@ -13,6 +16,31 @@ server.listen(process.env.PORT || 3000);
 
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
-    res.sendStatus(200);
-    console.log(req.body);
+  // 先行してLINE側にステータスコード200でレスポンスする。
+  res.sendStatus(200);
+
+  // すべてのイベント処理のプロミスを格納する配列。
+  let events_processed = [];
+
+  // イベントオブジェクトを順次処理。
+  req.body.events.forEach((event) => {
+      // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
+      if (event.type == "message" && event.message.type == "text"){
+          // ユーザーからのテキストメッセージが「こんにちは」だった場合のみ反応。
+          if (event.message.text == "こんにちは"){
+              // replyMessage()で返信し、そのプロミスをevents_processedに追加。
+              events_processed.push(bot.replyMessage(event.replyToken, {
+                  type: "text",
+                  text: "これはこれは"
+              }));
+          }
+      }
+  });
+
+  // すべてのイベント処理が終了したら何個のイベントが処理されたか出力。
+  Promise.all(events_processed).then(
+      (response) => {
+          console.log(`${response.length} event(s) processed.`);
+      }
+  );
 });
