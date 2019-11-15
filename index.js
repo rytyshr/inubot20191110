3ad0775e24369a98ea4fdb50608a663891748271
yasuraha <sabu9227@gmail.com>
2019-11-15 23:14:28 +0900

    Improve

--- index.js
+++ index.js
@@ -5,18 +5,11 @@ const server = require("express")();
 const line = require("@line/bot-sdk"); // Messaging APIのSDKをインポート
 const rp = require('request-promise');
 
-// メッセージ
-const message = {
-  type: "text",
-  text: "取り急ぎの犬です"
-}
-
 // パラメータ設定
 const line_config = {
     channelAccessToken: process.env.LINE_ACCESS_TOKEN, // 環境変数からアクセストークンをセットしています
     channelSecret: process.env.LINE_CHANNEL_SECRET // 環境変数からChannel Secretをセットしています
 };
-
 // APIコールのためのクライアントインスタンスを作成
 const bot = new line.Client(line_config);
 
@@ -26,15 +19,28 @@ const options = {
   method: "GET",
   json: true
 }
+// メッセージ（成功時）
+const message_successed = {
+  type: "text",
+  text: "取り急ぎの犬です"
+}
+// メッセージ（失敗時）
+const message_failed = {
+  type: "text",
+  text: "いいのがちょっとないので後ほどメッセージをください"
+}
+// メッセージ（注意）
+const message_warmed = {
+  type: "text",
+  text: "メッセージを送ってみて"
+}
 
 // Webサーバー設定
 server.listen(process.env.PORT || 3000);
-
 // ルーター設定
 server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
   // 先行してLINE側にステータスコード200でレスポンスする。
   res.sendStatus(200);
-  
   // イベントオブジェクトを順次処理。
   req.body.events.forEach((event) => {
     // この処理の対象をイベントタイプがメッセージで、かつ、テキストタイプだった場合に限定。
@@ -53,20 +59,14 @@ server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
           }
         })
         .then(function (image) {
-          bot.replyMessage(event.replyToken, [message, image]);
+          bot.replyMessage(event.replyToken, [message_successed, image]);
         })
         .catch(function (err) {
           console.dir(err);
-          bot.replyMessage(event.replyToken, {
-            type: "text",
-            text: "いいのがちょっとないので後ほどメッセージをください..."
-          });
+          bot.replyMessage(event.replyToken, message_failed);
         });
     } else {
-      bot.replyMessage(event.replyToken, {
-        type: "text",
-        text: "メッセージを送ってみて"
-      });
+      bot.replyMessage(event.replyToken, message_warmed);
     }
   });
 });
